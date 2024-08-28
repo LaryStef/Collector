bc60c9ce892d1afd1145231b95c8cac0f514e08b
LaryStef <timurkotov1999@gmail.com>
2024-08-28 18:28:46 +0300

    feat: celery app

--- celeryconfig.py
+++ celeryconfig.py
@@ -12,12 +12,8 @@ broker_transport_options: dict[str, int] = {
     "visibility_timeout": 43200
 }
 beat_schedule: dict[str, dict[str, str | crontab]] = {
-        "clear-postgre": {
-            "task": "delete_expired_sessions",
-            "schedule": crontab(minute="0", hour="*/12")
-        },
-        "clear-redis": {
-            "task": "delete_expired_applications",
-            "schedule": crontab(minute="0", hour="*/12")
+        "collect-data": {
+            "task": "collect_prices",
+            "schedule": crontab(minute=0)
         }
 }
