8cd3712d0d9d30630d3370e8cc2b793df8b44684
LaryStef <timurkotov1999@gmail.com>
2024-08-30 11:35:23 +0300

    chore: config

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,19 +1,21 @@
 from celery.schedules import crontab
 
 
+imports: list[str] = ["src.tasks"]
 broker_url: str = "redis://localhost:6379/0"
 task_ignore_result: bool = True
 task_time_limit: int = 10
 timezone: str = "UTC"
-worker_logfile: str = "cryptodeal.log"
-worker_hijack_root_logger: bool = False
-worker_redirect_stdouts: bool = True
+# worker_logfile: str = "cryptodeal.log"
+# worker_hijack_root_logger: bool = False
+# worker_redirect_stdouts: bool = True
+broker_connection_retry_on_startup: bool = True
 broker_transport_options: dict[str, int] = {
     "visibility_timeout": 43200
 }
 beat_schedule: dict[str, dict[str, str | crontab]] = {
         "collect-data": {
-            "task": "collect_prices",
-            "schedule": crontab(minute=0)
+            "task": "src.tasks.collect_prices",
+            "schedule": 30  # crontab(minute=0)
         }
 }
