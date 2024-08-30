e510b8ffd4f8ee933257bd86e3dec1e293bcba6d
LaryStef <timurkotov1999@gmail.com>
2024-08-30 11:31:04 +0300

    feat: collect prices task

---
+++ src/tasks.py
@@ -0,0 +1,39 @@
+import requests
+
+from pydantic import ValidationError
+
+from . import celery
+from .models import CryptoCourse
+from .config import API_KEY
+
+
+@celery.task(
+    bind=True,
+    max_retries=3,
+    default_retry_delay=30,
+    time_limit=15
+)
+def collect_prices(self) -> None:
+    # TODO get cryptocurrency tickers
+
+    tickers = [
+        "btc",
+        "eth",
+        "sol"
+    ]
+
+    r: requests.Response = requests.get(url=f"https://www.worldcoinindex.com/apiservice/ticker?key={API_KEY}&label={"btc-".join(tickers) + "btc"}&fiat=usd")  # noqa E501
+
+    if (r.status_code != 200):
+        self.retry()
+
+    try:
+        courses: CryptoCourse = CryptoCourse.model_validate_json(r.text)
+    except ValidationError:
+        self.retry()
+
+    for currency in courses.Markets:
+        # TODO save price
+
+        import pprint
+        pprint.pprint(currency)
