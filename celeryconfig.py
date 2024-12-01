c9e36cc8aeb8063908899f7967d9bfd4ad5b864e
LaryStef <timurkotov1999@gmail.com>
2024-12-01 15:00:29 +0300

    feat: Using env vars for db connection

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,17 +1,16 @@
-from celery.schedules import crontab
+from os import getenv
 
+from celery.schedules import crontab
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 imports: list[str] = ["src.tasks"]
-broker_url: str = "redis://localhost:6379/0"
+broker_url: str = f"redis://{getenv('REDIS_USER')}:{getenv('REDIS_PASSWORD')}@host.docker.internal:6379/0"  # noqa: E501
 task_ignore_result: bool = True
 task_time_limit: int = 10
 timezone: str = "UTC"
-
-# worker_logfile: str = "./logs/app.log"
-# worker_hijack_root_logger: bool = False
-# worker_redirect_stdouts: bool = True
-# worker_log_format = "[%(asctime)s] [%(levelname)s] [%(task_name)s] %(message)s"
-
 broker_connection_retry_on_startup: bool = True
 broker_transport_options: dict[str, int] = {
     "visibility_timeout": 43200
--- src/config.py
+++ src/config.py
@@ -7,8 +7,9 @@ load_dotenv()
 
 
 class Settings():
-    API_KEY: str = getenv("api_key", "")
-    DB_URL: str = f"postgresql://postgres:{getenv('database_password')}@localhost:5432/postgres"  # noqa: E501
+    API_KEY: str = getenv("API_KEY", "")
+    DB_URL: str = f"postgresql://{getenv('POSTGRES_USER')}:{getenv('POSTGRES_PASSWORD')}@host.docker.internal:5432/{getenv('POSTGRES_NAME')}"  # noqa: E501
+
     BACKUP_COUNT: int = 3
     MAX_BYTES_PER_FILE: int = 4 * 1024 * 1024
 
