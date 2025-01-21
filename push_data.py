1dcfcbdd3d3b5ec98e237e84757509ede1711f77
lary_stef <timurkotov1999@gmail.com>
2025-01-21 15:32:31 +0300

    feat: Script for pushing data to db directly

---
+++ push_data.py
@@ -0,0 +1,226 @@
+import os
+from random import uniform, randint
+from string import ascii_letters, digits
+
+from sqlalchemy import create_engine, Engine, MetaData
+from sqlalchemy.orm import Session
+
+from src.database.models import CryptoCourse, CryptoCurrency, Fiat
+
+
+SQLALCHEMY_DATABASE_URI: str = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost:5432/{os.getenv('POSTGRES_NAME')}"  # noqa: E501
+
+engine: Engine = create_engine(SQLALCHEMY_DATABASE_URI)
+metadata: MetaData = MetaData()
+
+
+with Session(engine) as session:
+    metadata.create_all(bind=engine)
+
+    def generate_id(length: int) -> str:
+        symbols: str = ascii_letters + digits
+        _id: str = ""
+
+        for _ in range(length):
+            _id += symbols[randint(0, 61)]
+        return _id
+
+    def push_day_course(min_: float, max_: float, ticker: str):
+        for hour in range(24):
+            session.add(
+                CryptoCourse(
+                    ID=generate_id(16),
+                    ticker=ticker,
+                    type_="hour",
+                    number=hour,
+                    price=round(uniform(min_, max_), ndigits=2)
+                )
+            )
+
+    def push_month_course(min_: float, max_: float, ticker: str):
+        for day in range(1, 32):
+            session.add(
+                CryptoCourse(
+                    ID=generate_id(16),
+                    ticker=ticker,
+                    type_="day",
+                    number=day,
+                    price=round(uniform(min_, max_), ndigits=2)
+                )
+            )
+
+    def push_year_course(min_: float, max_: float, ticker: str):
+        for month in range(1, 13):
+            session.add(
+                CryptoCourse(
+                    ID=generate_id(16),
+                    ticker=ticker,
+                    type_="month",
+                    number=month*2,
+                    price=round(uniform(min_, max_), ndigits=2)
+                )
+            )
+            session.add(
+                CryptoCourse(
+                    ID=generate_id(16),
+                    ticker=ticker,
+                    type_="month",
+                    number=month*2-1,
+                    price=round(uniform(min_, max_), ndigits=2)
+                )
+            )
+
+    session.add(
+        CryptoCurrency(
+            ticker="BTC",
+            name="Bitcoin",
+            description="""
+                <h3>Overview</h3>
+                <p>
+                    Bitcoin (BTC) is the world's first globally viable
+                    cryptocurrency built with blockchain technology. Outlined
+                    in 2008 by an anonymous developer under the pseudonym
+                    Satoshi Nakamoto, Bitcoin remains the most widely accepted
+                    and traded cryptocurrency today. Nakamoto conceived
+                    Bitcoin as a peer-to-peer electronic cash system that had
+                    no need for a central authority or single administrator. A
+                    global team of developers continues to maintain and work
+                    on the improvement of the Bitcoin protocol.
+                </p>
+                <h3>Who created Bitcoin?</h3>
+                <p>
+                    An unknown programmer published the Bitcoin white paper
+                    under the pseudonym "Satoshi Nakamoto" in 2008. Satoshi
+                    Nakamoto may be an individual or a group of people.
+                    Despite the widespread use and popularity of Bitcoin, the
+                    true identity of Satoshi Nakamoto remains a mystery. Over
+                    the years, many people have claimed to be the real Satoshi
+                    Nakamoto, but none of them have been able to provide
+                    definitive evidence to support their claims. Whoever
+                    Nakamoto is or was, they went to great lengths to remain
+                    anonymous. This mystery has helped increase the appeal of
+                    bitcoin as a global currency and fascination surrounding
+                    the origins of Bitcoin. Those closely related to
+                    cryptography around the time of Bitcoin's conception
+                    remain the most prominent suspects. These include computer
+                    programmers Nick Szabo and the late Hal Finney. Miners
+                    created the Bitcoin genesis block on January 3, 2009.
+                </p>""",
+            volume=28_388_811_901.29
+        )
+    )
+    push_day_course(38928.0, 82931.0, "BTC")
+    push_month_course(33928.0, 89931.0, "BTC")
+    push_year_course(27928.0, 101931.0, "BTC")
+
+    session.add(
+        CryptoCurrency(
+            ticker="ETH",
+            name="Ethereum",
+            description="""
+                <h3>Overview</h3>
+                <p>
+                    Ethereum is a decentralized, open-source blockchain
+                    platform established in 2013. Unlike Bitcoin, which
+                    focuses mainly on digital payments, Ethereum empowers
+                    developers to create decentralized applications (dApps)
+                    for diverse purposes, such as finance, gaming, and supply
+                    chain management. Smart contracts, which are
+                    self-executing programs containing the terms of agreements
+                    in code, underpin these applications. Ethereum was the
+                    first platform to introduce smart contract functionality.
+                    It operates using Ether (ETH), its native cryptocurrency,
+                    necessary for executing smart contracts and transactions
+                    on the network. Ether, the second largest cryptocurrency
+                    by market capitalization, can be traded on exchanges and
+                    serves as a store of value, akin to Bitcoin. Users of the
+                    Ethereum blockchain pay gas fees, denominated in ETH, for
+                    transaction validation.
+                </p>
+                <h3>Creator</h3>
+                <p>
+                    Vitalik Buterin, a Russian-Canadian programmer, created
+                    Ethereum. At just 19, he recognized the limitations of
+                    centralized systems after a frustrating change to his
+                    favorite World of Warcraft character. This inspired his
+                    vision of a decentralized digital network enabling the
+                    development of applications interacting with digital
+                    currencies. Before Ethereum, Buterin co-founded Bitcoin
+                    Magazine, one of the earliest Bitcoin publications. He
+                    published the Ethereum white paper in 2014 and launched
+                    the project in 2015, mining the genesis block on July 30,
+                    2015. Buterin’s vision attracted several passionate
+                    co-founders, including Gavin Wood, Joseph Lubin, Anthony
+                    Di Iorio, and Charles Hoskinson. Together, they founded
+                    the Ethereum Foundation, a non-profit organization aimed
+                    at supporting the platform's development and ecosystem.
+                    Notably, only Buterin remains with the project from the
+                    original co-founders.
+                </p>
+                <a href="https://www.youtube.com/shorts/LtrgP17dw_E">
+                    His best video ever.
+                </a>""",
+            volume=7_865_323_997.26
+        )
+    )
+    push_day_course(2373.0, 2963.0, "ETH")
+    push_month_course(2173.0, 3263.0, "ETH")
+    push_year_course(1973.0, 3463.0, "ETH")
+
+    session.add(
+        CryptoCurrency(
+            ticker="USDT",
+            name="Tether",
+            description="""
+                <h3>Overview</h3>
+                <p>
+                    Tether (USDT) is a stablecoin, which is a type of
+                    cryptocurrency that actively works to keep its valuation
+                    stable through market mechanisms. It’s used by investors
+                    who want to hedge against the inherent volatility of their
+                    cryptocurrency investments while still keeping value
+                    inside the crypto market, ready to be used without hassle.
+                    Tether is a fiat-collateralized stablecoin, which is a
+                    type of stablecoin that is backed by a fiat currency like
+                    USD, CAD, AUD, or even Yen (JPY). Tether was created to
+                    bridge the gaps between fiat currencies and blockchain
+                    assets while offering transparency, stability, and low
+                    fees for USDT users. Tether is pegged against the U.S.
+                    Dollar at a 1:1 ratio. There is no guarantee from Tether
+                    Ltd. for any right of redemption or exchange of Tether to
+                    USD. USDT cannot be exchanged directly for USD through the
+                    Tether company.
+                </p>
+                <h3>How does Tether work?</h3>
+                <p>
+                    Each Tether issued is backed by one US dollar worth
+                    of assets. All Tether was initially issued on the Bitcoin
+                    blockchain via the Omni Layer protocol, but can now be
+                    issued on any chain that Tether currently supports. Once a
+                    tether (a single unit of USDT) has been issued, it can be
+                    used the same as any other currency or token on the chain
+                    that it has been issued on. Tether currently supports the
+                    Bitcoin, Ethereum, EOS, Tron, Algorand, and OMG Network
+                    blockchains. Tether uses Proof Of Reserves, which means
+                    that at any time their reserves will be equal to or
+                    greater than the number of Tether in circulation. This can
+                    be verified via their website.
+                </p>""",
+            volume=42_177_103_848.39
+        )
+    )
+    push_day_course(0.985, 1.015, "USDT")
+    push_month_course(0.985, 1.015, "USDT")
+    push_year_course(0.985, 1.015, "USDT")
+
+    session.add(
+        Fiat(
+            iso="USD",
+            name="United States Dollar",
+            description="dollar description",
+            volume=1_000_000.37
+        )
+    )
+
+    session.commit()
+    print("Data pushed successfully.")
